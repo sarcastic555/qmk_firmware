00900be99b5db617e9b168e0f7d7b3ba7d391a3f
sarcastic555 <Unkown>
2020-05-03 11:33:57 +0900

    update keymap definition

--- keyboards/helix/rev2/keymaps/mine/keymap.c
+++ keyboards/helix/rev2/keymaps/mine/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGDN, KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT , \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGDN, KC_PGUP, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT , \
       KC_LCTL,ADJUST,KC_LALT, _______,    EISU,    KC_SPC,  LOWER,   RAISE,   KC_SPC,  KANA,    _______, KC_LEFT, KC_DOWN, KC_RGHT \
       ),
 
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   +  |   -  |   =  |   /  |   \  |      |
+   * |   ~  |   '  |   "  |   `  |   $  |   %  |             |   +  |   -  |   =  |   /  |   \  |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |   *  |   /  |   =  |   |  |   :  | Ent  |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT( \
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_PPLS, KC_PMNS,  KC_EQL, KC_SLSH, KC_BSLS, _______, \
+      KC_TILD, KC_QUOT, KC_DQUO, KC_GRV,  KC_DLR,  KC_PERC,                   KC_PPLS,  KC_PMNS,  KC_EQL, KC_SLSH, KC_BSLS, _______, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_ASTR, KC_SLSH,  KC_EQL, KC_PIPE, KC_COLN, KC_ENT, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_UNDS, KC_PMNS,  KC_TILD, _______, _______, _______, \
       _______, KC_HENK, KC_MHEN, KC_LANG3,KC_LANG4,KC_LANG5, _______, _______, _______, _______, _______, _______, _______, _______ \
