e8c42ef43fff8b4585678d076143d85dba45fec2
sarcastic555 <Unkown>
2020-05-07 23:03:52 +0900

    update keymap

--- keyboards/helix/rev2/keymaps/mine/keymap.c
+++ keyboards/helix/rev2/keymaps/mine/keymap.c
@@ -63,17 +63,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |PageDn|PageUp|   N  |   M  |   ,  |   .  |  Up  |Enter |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |PageUp|PageDn|   N  |   M  |   ,  |   .  |  Up  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Ctrl |Adjust| Alt  |      | EISU |Space |Lower |Raise |Space | KANA |      | Left | Down |Right |
+   * | Ctrl |Adjust| Alt  |      |      |HENKAN|Lower |Raise |Space |MUHENK|      | Left | Down |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGDN, KC_PGUP, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT , \
-      KC_LCTL,ADJUST,KC_LALT, _______,    EISU,    KC_SPC,  LOWER,   RAISE,   KC_SPC,  KANA,    _______, KC_LEFT, KC_DOWN, KC_RGHT \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGUP, KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT , \
+      KC_LCTL,ADJUST,KC_LALT, _______,    KC_HENK, KC_SPC,  LOWER,   RAISE,   KC_SPC,  KC_MHEN, _______, KC_LEFT, KC_DOWN, KC_RGHT \
       ),
 
   /* Colemak
@@ -122,21 +122,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |   ~  |   '  |   "  |   `  |   $  |   %  |             |   +  |   -  |   =  |   /  |   \  |      |
+   * |   ~  |   '  |   "  |   `  |   $  |   %  |             |   +  |   -  |   =  |   {  |   }  |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |   *  |   /  |   =  |   |  |   :  | Ent  |
+   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |   *  |   /  |   |  |   [  |   ]  | Ent  |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  (   |   )  |   _  |   -  |   ~  |      |      |      |
+   * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  (   |   )  |   _  |   -  |   ~  |   <  |   >  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
+   * |      |      |      |      |      |      |      |      |   ?  |      |      |   /  |   \  |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-      KC_TILD, KC_QUOT, KC_DQUO, KC_GRV,  KC_DLR,  KC_PERC,                   KC_PPLS,  KC_PMNS,  KC_EQL, KC_SLSH, KC_BSLS, _______, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_ASTR, KC_SLSH,  KC_EQL, KC_PIPE, KC_COLN, KC_ENT, \
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_UNDS, KC_PMNS,  KC_TILD, _______, _______, _______, \
-      _______, KC_HENK, KC_MHEN, KC_LANG3,KC_LANG4,KC_LANG5, _______, _______, _______, _______, _______, _______, _______, _______ \
+      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+      KC_TILD, KC_QUOT, KC_DQUO, KC_GRV,  KC_DLR,  KC_PERC,                   KC_PPLS, KC_PMNS,  KC_EQL,  KC_LCBR, KC_RCBR, _______, \
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_ASTR, KC_SLSH,  KC_PIPE, KC_LBRC, KC_RBRC, KC_ENT, \
+      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_LPRN,KC_RPRN, KC_UNDS, KC_PMNS,  KC_TILD, KC_LABK, KC_RABK, _______, \
+      _______, KC_HENK, KC_MHEN, KC_LANG3,KC_LANG4,KC_LANG5, _______,_______, KC_QUES, _______,  _______, KC_SLSH, KC_BSLS, _______ \
       ),
 
   /* Raise
